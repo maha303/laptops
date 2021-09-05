34ab3baa305cc71ecf322cc594bac6b82f31757f
maha303 <86849690+maha303@users.noreply.github.com>
2021-09-05 07:21:45 +0300

    22

---
+++ Laptops.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LAPTOP_STORE22.Models
+{
+    public class Laptops
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required ")]
+        [Display(Name ="Laptops Name "),MinLength(2,ErrorMessage ="Minimum lenght is 2 chars ")]
+        public String Name { get; set; }
+        public string Brand { get; set; }
+        [DataType(DataType.Currency,ErrorMessage ="Please enter correct price.")]
+        public  double Price { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ Order.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LAPTOP_STORE22.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage="Select Laptop")]
+        public int LaptopId { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [DataType(DataType.Text)]
+        public int ClientName { get; set; }
+
+        [Required(ErrorMessage = "street is required")]
+        [DataType(DataType.Text)]
+        public int Street { get; set; }
+
+        [Required(ErrorMessage = "City is required")]
+        [DataType(DataType.Text)]
+        public int City { get; set; }
+
+        public int State { get; set; }
+
+        [Required(ErrorMessage = "Country is required")]
+        [DataType(DataType.Text)]
+        public int Country { get; set; }
+
+        [Required(ErrorMessage = "ZipCode is required")]
+        [DataType(DataType.PostalCode)]
+        public int Zip { get; set; }
+
+        [Required(ErrorMessage = "Phone is required")]
+        [DataType(DataType.PhoneNumber)]
+        public int Phone { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [DataType(DataType.EmailAddress)]
+        public int Email { get; set; }
+
+
+    }
+}
