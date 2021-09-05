10ddb863c9f26a387f1efa79833a1280b157df30
maha303 <86849690+maha303@users.noreply.github.com>
2021-09-05 07:24:09 +0300

    66

---
+++ HomeInsexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using LAPTOP_STORE22.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LAPTOP_STORE22.ViewModels
+{
+    public class HomeInsexViewModel
+    {
+        public IEnumerable<Laptops> laptops { get; set; }
+        public IEnumerable<Carousel> carousels { get; set; }
+    }
+}
---
+++ OrderViewModel.cs
@@ -0,0 +1,14 @@
+﻿using LAPTOP_STORE22.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LAPTOP_STORE22.ViewModels
+{
+    public class OrderViewModel
+    {
+        public Laptops LaptopToOrder { get; set; }
+        public Order OrderDetails { get; set; }
+    }
+}
